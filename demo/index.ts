3a7a4fa71a4c85c79892e5aa05f285b1c4eb2c67
Vasantha kumar K <75217476+vasanth525@users.noreply.github.com>
2022-10-22 14:17:18 +0530

    Add files via upload

---
+++ demo/index.ts
@@ -0,0 +1,12 @@
+//sample
+
+import { DropDownList } from "../src/DropDownList/Base/dropdownlist";
+
+let dataSource: string[] = ['vasanth', 'gokul', 'kumar', 'pappitha'];
+
+let sampleDD: DropDownList = new DropDownList({
+  dataSource: dataSource,
+  // selectedIndex: 2,
+  // placeHolder: "Select the customer..."
+});
+sampleDD.appendTo('dd');
---
+++ src/DropDownList/Base/dropdownlist.ts
@@ -0,0 +1,46 @@
+import { ComponentBase } from "../../Base/component";
+import { IDropDown } from "./interface";
+
+//component
+
+export class DropDownList extends ComponentBase {
+  public dropDown: IDropDown;
+  constructor(sampleObj: IDropDown, public element?: HTMLElement) {
+    super(element);
+    this.dropDown = sampleObj;
+  }
+
+  public render() {
+    this.createDD();
+  }
+
+  private createDD() {
+    let selectTag: HTMLElement = document.createElement('select');
+    selectTag.setAttribute('name', 'customers');
+    selectTag.style.width = '300px';
+    selectTag.style.height = '40px';
+    selectTag.style.border = 'none';
+    selectTag.setAttribute('id', 'customers');
+    if (!this.dropDown.selectedIndex) {
+      let optionTag: HTMLElement = document.createElement('option');
+      optionTag.setAttribute('value', '');
+      optionTag.setAttribute('selected', 'selected');
+      if (this.dropDown.placeHolder) {
+        optionTag.innerText = this.dropDown.placeHolder;
+      }
+      selectTag.appendChild(optionTag);
+    }
+    for (let i: number = 0; this.dropDown && this.dropDown.dataSource && i < this.dropDown.dataSource.length; i++) {
+      let optionTag: HTMLElement = document.createElement('option');
+      optionTag.setAttribute('value', this.dropDown.dataSource[i] + "");
+      if (this.dropDown.selectedIndex && this.dropDown.selectedIndex === i) {
+        optionTag.setAttribute('selected', 'selected');
+      }
+      optionTag.innerText = this.dropDown.dataSource[i] + "";
+      selectTag.appendChild(optionTag);
+    }
+    this.element = selectTag;
+  }
+}
+
+// Write TypeScript code!
